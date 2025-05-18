f57b65c414f41d58f3b8351d2bc0ed0c650e0d26
Isa Greco <120188328+isabellagreco1997@users.noreply.github.com>
2025-05-18 16:55:38 +0100

    Updated page.tsx

--- src/app/story/[id]/page.tsx
+++ src/app/story/[id]/page.tsx
@@ -247,7 +247,7 @@ export default function StoryPage() {
         </div>
 
         {/* Main Content */}
-        <div className="relative z-10 -mt-44">
+        <div className="relative z-10 -mt-16">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
               
